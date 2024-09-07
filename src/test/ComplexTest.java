9fc846fa46cbd5f90935000b010979f3929f5370
Krame1S <iliaha2005@gmail.com>
2024-09-07 17:46:13 +0300

    new: added unit test for new Complex operation

--- src/test/ComplexTest.java
+++ src/test/ComplexTest.java
@@ -1,4 +1,16 @@
 package test;
 
+import demo.parallel.Complex;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ComplexTest {
+    @Test
+    public void testAddition() {
+        Complex a = new Complex(1, 1);
+        Complex b = new Complex(2, 2);
+        a.plus(b);
+        assertEquals(3, a.getRe());
+        assertEquals(3, a.getIm());
+    }
 }
